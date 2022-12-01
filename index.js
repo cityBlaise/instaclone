d544168e001ff798ba7a4e7fcb2c1029aa791835
cityBlaise <cityjmo@gmail.com>
2022-12-01 16:21:14 +0100

    made the show password button works

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const FirstImage = document.getElementById("firstImg");
+const showpwdBtn = document.querySelector('.show-password');
+const inputPwd = document.getElementById('password');
 let current = FirstImage;
 let previous = undefined;
 setInterval(() => { 
@@ -7,3 +9,10 @@ setInterval(() => {
     current.classList.add('active'); 
 
 }, 10000);
+
+showpwdBtn.addEventListener('click',(e)=>{
+    showpwdBtn.innerText=(inputPwd.type =='password')?'Dissimuler':'Afficher';
+    inputPwd.type=(inputPwd.type =='password')?'text':'password';
+});
+
+showpwdBtn.addEventListener
